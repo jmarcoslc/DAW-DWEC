eae716530e4fa12f97cd3acfa83270bf7b1320e3
Jose Marcos Leyva Castro <jmarcoslc@gmail.com>
2016-10-17 15:59:44 +0200

    Hecho hasta la J

--- Tema 3/Relación 2.1/ej1.js
+++ Tema 3/Relación 2.1/ej1.js
@@ -4,7 +4,7 @@ function largestNumber(arr) {
 
 function longestString(arr) {
 	return arr.sort(function(a, b) {
-		return a > b;
+		return a.length - b.length || a > b;
 	})[arr.length-1];
 }
 
@@ -44,13 +44,45 @@ function findInChain(arr) {
 	return items;
 }
 
+function assert3(arr) {
+	return arr.every(function(valor) {
+		return valor % 3 == 0;
+	});
+}
+
+function zipArrays(arr1, arr2) {
+	return arr1.concat(arr2);
+}
+
+function chift(arr) {
+	var arr_2 = arr;
+	arr_2.shift();
+
+	return arr_2;
+}
+
+function unchift(arr, word) {
+	var arr_2 = arr;
+	arr_2.unshift(word);
+
+	return arr_2;
+}
+
 window.onload = function() {
 	var arr = [1,2,5,789,20];
-	var arr_str = ["12345", "ads", "asdasdad", "askdgaskjdgsakjhda asdas is"];
+	var arr_str = ["12345", "adasdasdasdasdasdasdsaasdasdsadsadasdsas", "asdasdad", "askdgaskjdgsakjhda asdas is"];
+	var joined = zipArrays(arr, arr_str);
 
 	document.getElementById("insert").innerHTML += largestNumber(arr) + "<br>";
 	document.getElementById("insert").innerHTML += longestString(arr_str) + "<br>";
 	document.getElementById("insert").innerHTML += evenNumbers(arr) + "<br>";
 	document.getElementById("insert").innerHTML += oddNumbers(arr) + "<br>";
 	document.getElementById("insert").innerHTML += findInChain(arr_str) + "<br>";
-}
+	document.getElementById("insert").innerHTML += assert3(arr) + "<br>";
+	document.getElementById("insert").innerHTML += zipArrays(arr, arr_str) + "<br>";
+	document.getElementById("insert").innerHTML += joined.sort(function(a, b) {
+		return a.length - b.length || a > b;
+	}) + "<br>";
+	document.getElementById("insert").innerHTML += chift(arr_str) + "<br>";
+	document.getElementById("insert").innerHTML += unchift(arr_str, "new word") + "<br>";
+}	
