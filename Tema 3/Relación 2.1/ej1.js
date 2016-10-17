a74b42a00ddd6456209b2cf3027a462f18d2d95f
Jose Marcos Leyva Castro <jmarcoslc@gmail.com>
2016-10-17 12:43:13 +0200

    Ejercicio C y D hecho

--- Tema 3/Relación 2.1/ej1.js
+++ Tema 3/Relación 2.1/ej1.js
@@ -8,10 +8,36 @@ function longestString(arr) {
 	})[arr.length-1];
 }
 
+function evenNumbers(arr) {
+	var arr_evens = new Array();
+
+	arr.forEach(function(item, index){
+		if (item % 2 == 1) {
+			arr_evens.push(item);
+		}
+	});
+
+	return arr_evens;
+}
+
+function oddNumbers(arr) {
+	var arr_odds = new Array();
+
+	arr.forEach(function(item, index){
+		if (item % 2 == 0) {
+			arr_odds.push(item);
+		}
+	});
+
+	return arr_odds;
+}
+
 window.onload = function() {
 	var arr = [1,2,5,789,20];
 	var arr_str = ["12345", "ads", "asdasdad", "askdgaskjdgsakjhda asdas ss"];
 
 	//document.getElementById("insert").innerHTML = largestNumber(arr);
-	document.getElementById("insert").innerHTML = longestString(arr_str);
+	//document.getElementById("insert").innerHTML = longestString(arr_str);
+	//document.getElementById("insert").innerHTML = evenNumbers(arr);
+	document.getElementById("insert").innerHTML = oddNumbers(arr);
 }
