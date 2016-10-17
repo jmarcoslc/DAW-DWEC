e1a81f9c1904bd6c586975fd24279fc4311392f3
Jose Marcos Leyva Castro <jmarcoslc@gmail.com>
2016-10-17 12:31:42 +0200

    Ejercicio B hecho

--- Tema 3/Relación 2.1/ej1.js
+++ Tema 3/Relación 2.1/ej1.js
@@ -2,14 +2,16 @@ function largestNumber(arr) {
 	return arr.sort()[arr.length-1];
 }
 
-function longestString() {
-
+function longestString(arr) {
+	return arr.sort(function(a, b) {
+		return a > b;
+	})[arr.length-1];
 }
 
 window.onload = function() {
 	var arr = [1,2,5,789,20];
 	var arr_str = ["12345", "ads", "asdasdad", "askdgaskjdgsakjhda asdas ss"];
 
-	document.getElementById("insert").innerHTML = largestNumber(arr);
 	//document.getElementById("insert").innerHTML = largestNumber(arr);
+	document.getElementById("insert").innerHTML = longestString(arr_str);
 }
